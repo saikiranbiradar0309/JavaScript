37a5064b07995bb2cb7448a7d66a9bef746c33ef
SaikiranBiradar <saikiranbiradar0309@gmail.com>
2026-09-23 12:49:07 +0530

    Data Types

---
+++ 01-DataTypes/primitive.js
@@ -0,0 +1,50 @@
+// Primitive Data Types
+
+// 1. Number
+// We store numeric values in a variable using the number data type. 
+// It can be an integer or a floating-point number.
+let a = 100;
+console.log(a);
+
+
+// 2. String
+// We store text values in a variable using the string data type.
+let b = "Hello Sai";
+console.log(b);
+
+
+// 3. Boolean (TRUE/FALSE)
+// We store boolean values in a variable using the boolean data type.   
+let c = true
+console.log(c);
+
+
+// 4. null
+// We store null values in a variable using the null data type.
+let d = null;
+console.log(d);
+
+
+// 5. undefined
+// We store undefined values in a variable using the undefined data type.
+let e;
+console.log(e);
+
+
+// 6. bigint
+// We store large integer values in a variable using the bigint data type.
+let f = 1234567890123456789012345678901234567890n;
+console.log(f);
+
+
+// 7. symbol
+// We store unique values in a variable using the symbol data type.
+let g = Symbol("Hello");
+console.log(g);
+
+
+
+// 8. object
+// We store complex data structures in a variable using the object data type.
+let h = { name: "Sai", age: 25 };
+console.log(h);
